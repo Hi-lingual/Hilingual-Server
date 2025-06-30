1669ad0f60c4c65c3199744faa04e6bbf849e33b
JIEUNI <jieundaeun@naver.com>
2025-06-30 15:38:01 +0900

    [init] #1 Topic Entity 구현

---
+++ src/main/java/org/hilingual/domain/Topic.java
@@ -0,0 +1,19 @@
+package org.hilingual.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Topic {
+    @Id
+    private LocalDate date;
+
+    @Column(length = 255)
+    private String topicEn;
+
+    @Column(length = 255)
+    private String topicKor;
+}
