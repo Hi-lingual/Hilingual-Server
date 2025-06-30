061162b60ecb2b7ad18833b011b68bb27cfe6285
JIEUNI <jieundaeun@naver.com>
2025-06-30 15:37:27 +0900

    [init] #1 환경변수 설정

--- src/main/java/org/hilingual/Main.java
+++ src/main/java/org/hilingual/Main.java
@@ -1,7 +1,22 @@
 package org.hilingual;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dotenv dotenv = Dotenv.configure()
+                .directory("src/main/resources")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(Main.class, args);
     }
 }
