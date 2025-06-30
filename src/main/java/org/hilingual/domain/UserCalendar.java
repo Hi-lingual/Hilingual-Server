008cd389daf9b4f4b8d50d8fede0e831862058af
JIEUNI <jieundaeun@naver.com>
2025-06-30 15:38:36 +0900

    [init] #1 UserCalendar Entity 구현

---
+++ src/main/java/org/hilingual/domain/UserCalendar.java
@@ -0,0 +1,23 @@
+package org.hilingual.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class UserCalendar {
+    @Id
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Boolean isWritten;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
+
