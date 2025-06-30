87bacf2e3f4f89e42b71eb4c34267503641dd971
JIEUNI <jieundaeun@naver.com>
2025-06-30 15:38:22 +0900

    [init] #1 User/UserProfile/Token Entity 구현

---
+++ src/main/java/org/hilingual/domain/Token.java
@@ -0,0 +1,17 @@
+package org.hilingual.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String refreshToken;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
