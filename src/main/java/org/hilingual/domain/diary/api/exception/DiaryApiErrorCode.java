be2bf87e04a54b16ad1c1d6babbab4bf9da7d712
choyeongju <jjudy1301@naver.com>
2025-07-01 17:38:44 +0900

    [feat] #3 Diary 관련 예외 코드 정의 (API/Core 분리)

---
+++ src/main/java/org/hilingual/domain/diary/api/exception/DiaryApiErrorCode.java
@@ -0,0 +1,29 @@
+package org.hilingual.domain.diary.api.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.hilingual.common.code.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum DiaryApiErrorCode implements ErrorCode {
+    ;
+
+    public final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus(){
+        return httpStatus;
+    }
+
+    @Override
+    public int getCode(){
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
