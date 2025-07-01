2f2a564e7a92299f3de544d93bbcaf0849832bd7
choyeongju <jjudy1301@naver.com>
2025-07-01 18:03:54 +0900

    [feat] #3 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/org/hilingual/Main.java
+++ src/main/java/org/hilingual/Main.java
@@ -4,7 +4,9 @@ package org.hilingual;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
