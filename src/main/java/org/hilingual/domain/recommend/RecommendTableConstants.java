ae099aa5f84d7e6cd7b6a0911be908b27972a64e
choyeongju <jjudy1301@naver.com>
2025-07-01 17:32:08 +0900

    [refac] #3 테이블 컬럼을 상수 클래스로 분리

---
+++ src/main/java/org/hilingual/domain/recommend/RecommendTableConstants.java
@@ -0,0 +1,11 @@
+package org.hilingual.domain.recommend;
+
+public class RecommendTableConstants {
+    public static final String TABLE_RECOMMEND = "recommend";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_PHRASE = "phrase";
+    public static final String COLUMN_PHRASE_TYPE = "phrase_type";
+    public static final String COLUMN_EXPLANATION = "explanation";
+    public static final String COLUMN_IS_MARKED = "is_marked";
+    public static final String COLUMN_DIARY_ID = "diary_id";
+}
---
+++ src/main/java/org/hilingual/domain/usesrprofile/UserProfileTableConstants.java
@@ -0,0 +1,11 @@
+package org.hilingual.domain.usesrprofile;
+
+public class UserProfileTableConstants {
+    public static final String TABLE_USER_PROFILE = "user_profile";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NICKNAME = "nickname";
+    public static final String COLUMN_PROFILE_IMG = "profile_img";
+    public static final String COLUMN_TOTAL_DIARIES = "total_diaries";
+    public static final String COLUMN_STREAK = "streak";
+    public static final String COLUMN_USER_ID = "user_id";
+}
