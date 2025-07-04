57e6a267cc2d7e917ceb270240c6525d1d15fe4d
choyeongju <jjudy1301@naver.com>
2025-07-04 14:09:26 +0900

    [feat] #3 기본 전역 예외 코드 추가

--- src/main/java/org/hilingual/common/code/GlobalErrorCode.java
+++ src/main/java/org/hilingual/common/code/GlobalErrorCode.java
@@ -5,6 +5,17 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
+    // 400
+    INVALID_ARGUMENTS(HttpStatus.BAD_REQUEST, 40000, "인자의 형식이 올바르지 않습니다."),
+
+    // 401
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 40100, "인증되지 않은 사용자입니다."),
+
+    // 405
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, 40500, "지원하지 않는 HTTP 메서드입니다."),
+
+    // 500
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 50000, "서버 내부 오류입니다."),
     ;
 
     public final HttpStatus httpStatus;
