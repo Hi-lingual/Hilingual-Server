63eb7b732e4037e88965737a10b36733542926d8
choyeongju <jjudy1301@naver.com>
2025-07-05 18:05:10 +0900

    [feat] #11 기본 예외 핸들링 setting

---
+++ src/main/java/org/hilingual/advice/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package org.hilingual.advice;
+
+import org.hilingual.common.dto.BaseResponseDto;
+import org.hilingual.common.exception.code.ErrorCode;
+import org.hilingual.common.exception.code.GlobalErrorCode;
+import org.hilingual.domain.diary.api.exception.DiaryBaseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DiaryBaseException.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleAuthBaseException(DiaryBaseException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(BaseResponseDto.fail(e.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponseDto<Map<String, String>>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(err ->
+                errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        return ResponseEntity
+                .status(GlobalErrorCode.INVALID_INPUT_VALUE.getHttpStatus())
+                .body(BaseResponseDto.fail(GlobalErrorCode.INVALID_INPUT_VALUE));
+    }
+
+    // 존재하지 않는 요청에 대한 예외
+    @ExceptionHandler(value = {NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity<BaseResponseDto<Void>> handleNoPageFoundException(Exception e) {
+        ErrorCode errorCode = e instanceof HttpRequestMethodNotSupportedException
+                ? GlobalErrorCode.METHOD_NOT_ALLOWED
+                : GlobalErrorCode.NOT_FOUND_END_POINT;
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(BaseResponseDto.fail(errorCode));
+    }
+
+    // 기본 예외
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleException(Exception e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(BaseResponseDto.fail(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+    }
+}
