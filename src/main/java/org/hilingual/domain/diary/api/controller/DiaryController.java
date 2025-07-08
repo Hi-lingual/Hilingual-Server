56dcc42612e3bc0a011b85f2356927b55131b7b5
choyeongju <jjudy1301@naver.com>
2025-07-09 01:40:33 +0900

    [feat] #23 일기 피드백 요청 API Controller 구현

--- src/main/java/org/hilingual/domain/diary/api/controller/DiaryController.java
+++ src/main/java/org/hilingual/domain/diary/api/controller/DiaryController.java
@@ -1,9 +1,13 @@
 package org.hilingual.domain.diary.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.hilingual.domain.diary.api.dto.res.DiaryDto;
+import org.hilingual.domain.diary.api.exception.DiaryApiErrorCode;
+import org.hilingual.domain.diary.api.exception.DiaryContentTooShortException;
 import org.hilingual.domain.diary.api.service.DiaryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiaryController {
 
     private final DiaryService diaryService;
+
+    @PostMapping(value = "/v1/diaries", consumes = "multipart/form-data")
+    public ResponseEntity<DiaryDto> getFeedbacks(
+            @RequestHeader("Authorization") Long userId,
+            @RequestPart("originalText") String originalText,
+            @RequestPart(value = "imageFile", required = false) MultipartFile imageFile
+    ) {
+        if(originalText.length() < 10){
+            throw new DiaryContentTooShortException(DiaryApiErrorCode.DIARY_TOO_SHORT);
+        }
+        return ResponseEntity.ok(diaryService.getFeedbacks(userId, originalText, imageFile));
+    }
 
 }
