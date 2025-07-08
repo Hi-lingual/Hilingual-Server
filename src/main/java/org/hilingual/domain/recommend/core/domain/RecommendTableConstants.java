55c490b44d3a8a35209a0c75ab1cc193e33bafdf
choyeongju <jjudy1301@naver.com>
2025-07-09 01:43:56 +0900

    [feat] #23 해당 표현을 추천한 이유를 알려주는 reason 필드 추가

--- src/main/java/org/hilingual/domain/recommend/core/domain/RecommendTableConstants.java
+++ src/main/java/org/hilingual/domain/recommend/core/domain/RecommendTableConstants.java
@@ -1,4 +1,4 @@
-package org.hilingual.domain.recommend;
+package org.hilingual.domain.recommend.core.domain;
 
 public class RecommendTableConstants {
     public static final String TABLE_RECOMMEND = "recommend";
@@ -6,6 +6,7 @@ public class RecommendTableConstants {
     public static final String COLUMN_PHRASE = "phrase";
     public static final String COLUMN_PHRASE_TYPE = "phrase_type";
     public static final String COLUMN_EXPLANATION = "explanation";
+    public static final String COLUMN_REASON = "string";
     public static final String COLUMN_IS_MARKED = "is_marked";
     public static final String COLUMN_DIARY_ID = "diary_id";
 }
