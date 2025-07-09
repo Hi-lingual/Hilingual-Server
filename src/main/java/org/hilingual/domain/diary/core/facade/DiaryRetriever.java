5dd2742d0f826c38868a4e8ce42f0343f170fc99
choyeongju <jjudy1301@naver.com>
2025-07-09 11:05:47 +0900

    [chore] #27-clean 충돌 해결

--- src/main/java/org/hilingual/domain/diary/core/facade/DiaryRetriever.java
+++ src/main/java/org/hilingual/domain/diary/core/facade/DiaryRetriever.java
@@ -1,6 +1,9 @@
 package org.hilingual.domain.diary.core.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.hilingual.domain.diary.core.domain.Diary;
+import org.hilingual.domain.diary.core.exception.DiaryCoreErrorCode;
+import org.hilingual.domain.diary.core.exception.DiaryNotFoundException;
 import org.hilingual.domain.diary.core.repository.DiaryRepository;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +12,10 @@ import org.springframework.stereotype.Component;
 public class DiaryRetriever {
 
     private final DiaryRepository diaryRepository;
+
+    public Diary findById(final long diaryId) {
+        return diaryRepository.findById(diaryId)
+                .orElseThrow(() -> new DiaryNotFoundException(DiaryCoreErrorCode.DIARY_NOT_FOUND));
+    }
 
 }
