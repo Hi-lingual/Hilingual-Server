667911b1f2db15e01a99b24a04f7113478518aad
choyeongju <jjudy1301@naver.com>
2025-07-09 16:27:37 +0900

    [fix] #31 올바른 권한 예외 던져주도록 수정

--- src/main/java/org/hilingual/domain/diaryfeedback/api/service/DiaryFeedbackService.java
+++ src/main/java/org/hilingual/domain/diaryfeedback/api/service/DiaryFeedbackService.java
@@ -3,6 +3,7 @@ package org.hilingual.domain.diaryfeedback.api.service;
 import lombok.RequiredArgsConstructor;
 import org.hilingual.domain.diary.core.domain.Diary;
 import org.hilingual.domain.diary.core.exception.DiaryCoreErrorCode;
+import org.hilingual.domain.diary.core.exception.DiaryForbiddenException;
 import org.hilingual.domain.diary.core.exception.DiaryNotFoundException;
 import org.hilingual.domain.diary.core.facade.DiaryRetriever;
 import org.hilingual.domain.diaryfeedback.api.dto.DiaryFeedbackList;
@@ -47,7 +48,7 @@ public class DiaryFeedbackService {
     private void validateDiaryOwnership(final long userId, final long diaryId) {
         Diary diary = diaryRetriever.findById(diaryId);
         if (!diary.getUser().getId().equals(userId)) {
-            throw new DiaryNotFoundException(DiaryCoreErrorCode.DIARY_FORBIDDEN);
+            throw new DiaryForbiddenException(DiaryCoreErrorCode.DIARY_FORBIDDEN);
         }
     }
 }
