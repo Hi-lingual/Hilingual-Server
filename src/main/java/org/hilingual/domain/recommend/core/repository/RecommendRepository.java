a653d948e6f8821f3dd6209a8e26dbbc063df160
bcachu <128598386+choyeongju@users.noreply.github.com>
2025-07-09 16:40:54 +0900

    [fix] #31 충돌 해결하면서 잘못한 파일 수정

--- src/main/java/org/hilingual/domain/recommend/core/repository/RecommendRepository.java
+++ src/main/java/org/hilingual/domain/recommend/core/repository/RecommendRepository.java
@@ -3,14 +3,10 @@ package org.hilingual.domain.recommend.core.repository;
 import org.hilingual.domain.recommend.core.domain.Recommend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< feat/31
 import java.util.List;
 
 public interface RecommendRepository extends JpaRepository<Recommend, Long> {
 
     List<Recommend> findByDiaryId(Long diaryId);
 
-=======
-public interface RecommendRepository extends JpaRepository<Recommend, Long> {
->>>>>>> develop
 }
