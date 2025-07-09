d0e27afc9272be2e92063681b21acee2070ed70c
JIEUNI <jieundaeun@naver.com>
2025-07-09 22:28:48 +0900

    [del] #21 TokenRepository 삭제

--- src/main/java/org/hilingual/domain/token/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hilingual.domain.token;
-
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.repository.CrudRepository;
-
-@EnableRedisRepositories(basePackages = "org.hilingual.domain.security")
-public interface TokenRepository extends CrudRepository<Token, Long> {
-}
