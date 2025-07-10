89e2fb1a62ba33a6b06be2f42eb6e29bb4111618
choyeongju <jjudy1301@naver.com>
2025-07-10 22:36:20 +0900

    [refac] #39 필요 없는 필드 삭제

--- src/main/java/org/hilingual/domain/diaryfeedback/core/repository/DiaryFeedbackRepository.java
+++ src/main/java/org/hilingual/domain/diaryfeedback/core/repository/DiaryFeedbackRepository.java
@@ -1,5 +1,6 @@
 package org.hilingual.domain.diaryfeedback.core.repository;
 
+
 import org.hilingual.domain.diaryfeedback.core.domain.DiaryFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
