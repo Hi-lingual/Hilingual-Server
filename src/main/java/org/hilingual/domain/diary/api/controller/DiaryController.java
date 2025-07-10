1b359b1585c807236082f64278f9764c3b1df156
choyeongju <jjudy1301@naver.com>
2025-07-10 22:19:04 +0900

    [feat] #39 AI가 쓴 일기 확인 API Controller

--- src/main/java/org/hilingual/domain/diary/api/controller/DiaryController.java
+++ src/main/java/org/hilingual/domain/diary/api/controller/DiaryController.java
@@ -1,10 +1,13 @@
 package org.hilingual.domain.diary.api.controller;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.hilingual.domain.diary.api.dto.res.DiaryDetails;
 import org.hilingual.domain.diary.api.dto.res.DiaryDto;
 import org.hilingual.domain.diary.api.exception.DiaryApiErrorCode;
 import org.hilingual.domain.diary.api.exception.DiaryContentTooShortException;
 import org.hilingual.domain.diary.api.service.DiaryService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,6 +29,14 @@ public class DiaryController {
             throw new DiaryContentTooShortException(DiaryApiErrorCode.DIARY_TOO_SHORT);
         }
         return ResponseEntity.ok(diaryService.getFeedbacks(userId, originalText, imageFile));
+    }
+
+    @GetMapping("/v1/diaries/{diaryId}")
+    public ResponseEntity<DiaryDetails> getDiaryDetails(
+            @RequestHeader("Authorization") Long userId,
+            @PathVariable("diaryId") @NotNull @Min(1) Long diaryId
+    ){
+        return ResponseEntity.ok(diaryService.getDiaryDetails(userId, diaryId));
     }
 
 }
