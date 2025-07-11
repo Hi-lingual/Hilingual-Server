7a51fa6b9d99a80c47982214087364ab7c6f7e27
Sihun23 <sihun23@gmail.com>
2025-07-11 15:46:00 +0900

    Update SecurityConstants.java

--- src/main/java/org/hilingual/domain/security/constant/SecurityConstants.java
+++ src/main/java/org/hilingual/domain/security/constant/SecurityConstants.java
@@ -4,6 +4,6 @@ public class SecurityConstants {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
     public static final String[] AUTH_WHITE_LIST = {
-        "/api/**", "/test/**", "/actuator/**"
+        "/api/**", "/test/**", "/actuator/**", "/h2-console/**"
     };
 }
