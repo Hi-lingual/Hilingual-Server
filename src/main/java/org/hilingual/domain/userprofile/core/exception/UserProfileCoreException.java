8d3471b091d7b5e63e2abeede528c0b1bb5ef968
Sihun23 <gcsw16@s-MackBook-Pro-16.local>
2025-07-13 22:54:26 +0900

    [fix] #52 잘못된 상속 class 수정

--- src/main/java/org/hilingual/domain/userprofile/core/exception/UserProfileCoreException.java
+++ src/main/java/org/hilingual/domain/userprofile/core/exception/UserProfileCoreException.java
@@ -1,11 +1,10 @@
 package org.hilingual.domain.userprofile.core.exception;
 
-import org.hilingual.common.exception.base.HilingualBaseException;
 import org.hilingual.common.exception.code.ErrorCode;
-import org.hilingual.domain.usercalendar.api.exception.UserCalendarBaseException;
+import org.hilingual.domain.userprofile.api.exception.UserProfileBaseException;
 import org.springframework.http.HttpStatus;
 
-public abstract class UserProfileCoreException extends UserCalendarBaseException {
+public abstract class UserProfileCoreException extends UserProfileBaseException {
     protected UserProfileCoreException(ErrorCode errorCode) {
         super(errorCode);
     }
