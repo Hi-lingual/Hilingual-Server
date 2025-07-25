e7664be3b4573182002a328ce9c8bcec188cc319
choyeongju <jjudy1301@naver.com>
2025-07-25 23:36:59 +0900

    [feat] #118 OpenAI API 요청용 FeignClient 인터페이스 추가

---
+++ src/main/java/org/hilingual/external/openai/client/OpenAIClient.java
@@ -0,0 +1,18 @@
+package org.hilingual.external.openai.client;
+
+import org.hilingual.external.feign.OpenAIFeignConfig;
+import org.hilingual.external.openai.OpenAIConstant;
+import org.hilingual.external.openai.dto.GptRequest;
+import org.hilingual.external.openai.dto.GptResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(value = "openAIClient", url = OpenAIConstant.OPENAI_URL, configuration = OpenAIFeignConfig.class)
+@Component
+public interface OpenAIClient {
+
+    @PostMapping
+    GptResponse callChatCompletion(GptRequest gptRequest);
+
+}
