8ae72b201a8708a112b72cd69dec9be4fa1d0bc6
Sihun23 <sihun23@gmail.com>
2025-08-06 04:36:22 +0900

    Update start-nginx.sh

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -4,4 +4,4 @@ rm -f /etc/nginx/conf.d/default.conf                 # 기본 welcome conf 제�
 # 템플릿의 $TARGET_UPSTREAM 값을 실제 IP:PORT 로 치환하여 conf 생성
 envsubst '$TARGET_UPSTREAM' < /etc/nginx/nginx.template.conf \
   > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'                               # foreground에 바로 실행
+nginx -g 'daemon off;'                               # foreground에 바로 실행!
