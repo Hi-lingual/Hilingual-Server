4a764063f503368c6ff018d4d974b0ca0da50b0d
choyeongju <jjudy1301@naver.com>
2025-08-14 23:56:54 +0900

    [feat] #143 ExceptionHandlerFilter 및 로깅 클래스 구현

---
+++ hilingual-auth/src/main/java/org/sopt/jwt/support/logsupport/ErrorResponder.java
@@ -0,0 +1,35 @@
+package org.sopt.jwt.support.logsupport;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.dto.BaseResponseDto;
+import org.sopt.exception.code.ErrorCode;
+import org.sopt.jwt.support.AuthConstant;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ErrorResponder {
+
+    private final ObjectMapper objectMapper;
+
+    public void write(HttpServletResponse response, ErrorCode errorCode) {
+        try {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(AuthConstant.CHARACTER_TYPE);
+            response.setStatus(errorCode.getHttpStatus().value());
+
+            PrintWriter writer = response.getWriter();
+            writer.write(objectMapper.writeValueAsString(BaseResponseDto.fail(errorCode)));
+        } catch (IOException e) {
+            log.error("Error writing response: {}", e.getMessage(), e);
+        }
+    }
+}
