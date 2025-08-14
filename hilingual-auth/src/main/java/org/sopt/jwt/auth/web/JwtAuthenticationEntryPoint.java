b6202ed2dad3bbf91dbe10214d44a607bedf81f9
choyeongju <jjudy1301@naver.com>
2025-08-14 23:59:49 +0900

    [feat] #143 AuthenticationException 처리 클래스

---
+++ hilingual-auth/src/main/java/org/sopt/jwt/auth/web/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,47 @@
+package org.sopt.jwt.auth.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.sopt.dto.BaseResponseDto;
+import org.sopt.exception.AuthErrorCode;
+import org.sopt.exception.code.ErrorCode;
+import org.sopt.jwt.support.AuthConstant;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * 401 전용 응답(AuthenticationException)
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException, ServletException {
+
+        ErrorCode errorCode = (ErrorCode) request.getAttribute("exception");
+
+        if (errorCode == null) {
+            errorCode = AuthErrorCode.WRONG_ENTRY_POINT;
+        }
+
+        response.setContentType(AuthConstant.CONTENT_TYPE);
+        response.setCharacterEncoding(AuthConstant.CHARACTER_TYPE);
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.getWriter().write(
+                objectMapper.writeValueAsString(BaseResponseDto.fail(errorCode))
+        );
+    }
+}
