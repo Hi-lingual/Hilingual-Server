e0e53b19711182e4af9e940407e426dd7df5101d
JIEUNI <jieundaeun@naver.com>
2025-08-15 21:11:01 +0900

    [chore] #141 noti -> notify로 수정

--- hilingual-domain/src/main/java/org/sopt/user/domain/UserTableConstants.java
+++ hilingual-domain/src/main/java/org/sopt/user/domain/UserTableConstants.java
@@ -9,7 +9,7 @@ public class UserTableConstants {
     public static final String COLUMN_IS_COMPLETED = "is_completed";
     public static final String COLUMN_IS_DELETED = "is_deleted";
     public static final String COLUMN_DELETED_AT = "deleted_at";
-    public static final String COLUMN_NOTIFY_STATUS = "noti_status";
+    public static final String COLUMN_NOTIFY_STATUS = "notify_status";
     public static final String COLUMN_REGISTER_STATUS = "register_status";
     public static final String COLUMN_USER = "user";
 }
