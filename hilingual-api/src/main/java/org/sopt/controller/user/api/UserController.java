9caaa182d90b0db7b728888392ee68d41c8e43be
choyeongju <jjudy1301@naver.com>
2025-08-19 02:17:51 +0900

    [feat] #158 Reissue 컨트롤러 추가

--- hilingual-api/src/main/java/org/sopt/controller/user/api/UserController.java
+++ hilingual-api/src/main/java/org/sopt/controller/user/api/UserController.java
@@ -1,9 +1,13 @@
 package org.sopt.controller.user.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.sopt.controller.user.dto.NicknameAvailableRes;
 import org.sopt.controller.user.service.UserService;
 import org.sopt.dto.BaseResponseDto;
+import org.sopt.jwt.core.JwtTokenProvider;
+import org.sopt.jwt.auth.dto.ReissueTokensRes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/profile")
     public BaseResponseDto<NicknameAvailableRes> getUserProfile(
             @RequestParam(value = "nickname") String nickname
     ) {
         return userService.getNicknameAvailable(nickname);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<ReissueTokensRes> reissue(
+            HttpServletRequest request
+    ){
+        String refreshToken = jwtTokenProvider.getJwtFromRequest(request);
+        return ResponseEntity.ok(userService.reissue(refreshToken));
     }
 }
