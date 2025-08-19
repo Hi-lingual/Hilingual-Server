01cc6a040e451af899e1b3f8361301ce1f38b5e0
choyeongju <jjudy1301@naver.com>
2025-08-19 17:00:44 +0900

    [feat] #161 유저 팔로우 등록 API 구현 완료

---
+++ hilingual-api/src/main/java/org/sopt/controller/follow/api/FollowController.java
@@ -0,0 +1,25 @@
+package org.sopt.controller.follow.api;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.controller.follow.service.FollowService;
+import org.sopt.jwt.annotation.UserId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/feed")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PutMapping("/{followId}/follow")
+    public ResponseEntity<Void> follow(
+            @UserId Long userId,
+            @PathVariable long followId
+    ){
+        followService.follow(userId, followId);
+        return ResponseEntity.ok().build();
+    }
+
+}
