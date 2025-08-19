e864d65c418ac3326bbcf89fc2518d628c81205e
choyeongju <jjudy1301@naver.com>
2025-08-19 19:06:18 +0900

    [feat] #161 unfollow 및 unfollow 이후 상태 조회 메서드 구현

--- hilingual-domain/src/main/java/org/sopt/follow/repository/FollowRepository.java
+++ hilingual-domain/src/main/java/org/sopt/follow/repository/FollowRepository.java
@@ -3,9 +3,24 @@ package org.sopt.follow.repository;
 import org.sopt.follow.domain.Follow;
 import org.sopt.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFollowerAndFollowee(User follower, User followee);
 
+    // unfollow 이후 상태 계산용 (you -> me)
+    boolean existsByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
+
+    // unfollow
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(
+            "delete from Follow f " +
+                    "where f.follower.id = :followerId " +
+                    "and f.followee.id = :followeeId"
+    )
+    int deleteByFollowerIdAndFolloweeId(@Param("followerId") Long followerId,
+                                        @Param("followeeId") Long followeeId);
 }
