3f3d1721272f7ce7c58ea1d46f5454feee2b6e68
JIEUNI <jieundaeun@naver.com>
2025-08-20 04:13:46 +0900

    [feat] #155 FeedProfileRes 구현

---
+++ hilingual-api/src/main/java/org/sopt/controller/feed/dto/FeedProfileRes.java
@@ -0,0 +1,35 @@
+package org.sopt.controller.feed.dto;
+
+import org.sopt.userprofile.domain.UserProfile;
+
+public record FeedProfileRes (
+        Boolean isMine,
+        String profileImg,
+        String nickname,
+        Integer follower,
+        Integer following,
+        Integer streak,
+        Boolean isFollowing,
+        Boolean isFollowed,
+        Boolean isBlocked
+){
+    public static FeedProfileRes from(
+            UserProfile profile,
+            Boolean isMine,
+            Boolean isFollowing,
+            Boolean isFollowed,
+            Boolean isBlocked
+    ) {
+        return new FeedProfileRes(
+                isMine,
+                profile.getProfileImg(),
+                profile.getNickname(),
+                profile.getFollowerCount(),
+                profile.getFollowingCount(),
+                profile.getStreak(),
+                isFollowing,
+                isFollowed,
+                isBlocked
+        );
+    }
+}
