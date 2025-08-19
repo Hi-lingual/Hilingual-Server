e9f59924741aa6f2436c5350064c0be518620697
choyeongju <jjudy1301@naver.com>
2025-08-19 16:50:03 +0900

    [feat] #161 Follow 엔티티에 유니크 제약 추가

--- hilingual-domain/src/main/java/org/sopt/follow/domain/Follow.java
+++ hilingual-domain/src/main/java/org/sopt/follow/domain/Follow.java
@@ -8,7 +8,16 @@ import lombok.NoArgsConstructor;
 import org.sopt.user.domain.User;
 
 @Entity
-@Table(name = FollowTableConstants.TABLE_FOLLOW)
+@Table(
+        name = FollowTableConstants.TABLE_FOLLOW,
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_follower_followee",
+                columnNames = {
+                        FollowTableConstants.COLUMN_FOLLOWER_ID,
+                        FollowTableConstants.COLUMN_FOLLOWEE_ID
+                }
+        )
+)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -26,6 +35,7 @@ public class Follow {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = FollowTableConstants.COLUMN_FOLLOWEE_ID, nullable = false)
     private User followee;
+
 
     public static Follow create(User follower, User followee) {
         return new Follow(null, follower, followee);
