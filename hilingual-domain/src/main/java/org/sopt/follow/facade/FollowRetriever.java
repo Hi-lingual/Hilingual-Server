d6858a53a11ecffb916d7164c9d9238c8e9bc0de
choyeongju <jjudy1301@naver.com>
2025-08-19 16:53:29 +0900

    [feat] #161 이미 팔로우된 관계일 경우 예외 발생하도록 검증 로직 추가

---
+++ hilingual-domain/src/main/java/org/sopt/follow/facade/FollowRetriever.java
@@ -0,0 +1,23 @@
+package org.sopt.follow.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.follow.exception.FollowAlreadyExistsException;
+import org.sopt.follow.exception.FollowCoreErrorCode;
+import org.sopt.follow.repository.FollowRepository;
+import org.sopt.user.domain.User;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class FollowRetriever {
+
+    private final FollowRepository followRepository;
+
+    @Transactional(readOnly = true)
+    public void assertNotFollowing(User follower, User followee) {
+        if (followRepository.existsByFollowerAndFollowee(follower, followee)) {
+            throw new FollowAlreadyExistsException(FollowCoreErrorCode.FOLLOW_ALREADY_EXISTS);
+        }
+    }
+}
