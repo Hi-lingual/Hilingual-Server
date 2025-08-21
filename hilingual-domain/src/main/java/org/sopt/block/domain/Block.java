d9def6aacf52839e904876ae0d28f67364f89d24
JIEUNI <jieundaeun@naver.com>
2025-08-21 21:30:45 +0900

    [chore] #167 도메인 static 상수 한번에 import하는 것으로 수정

--- hilingual-domain/src/main/java/org/sopt/block/domain/Block.java
+++ hilingual-domain/src/main/java/org/sopt/block/domain/Block.java
@@ -8,15 +8,17 @@ import lombok.NoArgsConstructor;
 import org.sopt.common.config.BaseTimeEntity;
 import org.sopt.user.domain.User;
 
+import static org.sopt.block.domain.BlockTableConstants.*;
+
 @Entity
 @Table(
-        name = BlockTableConstants.TABLE_BLOCK,
+        name = TABLE_BLOCK,
         uniqueConstraints = {
         @UniqueConstraint(
-                name = BlockTableConstants.UK_BLOCKER_BLOCKED,
+                name = UK_BLOCKER_BLOCKED,
                 columnNames = {
-                        BlockTableConstants.COLUMN_BLOCKER_ID,
-                        BlockTableConstants.COLUMN_BLOCKED_ID
+                        COLUMN_BLOCKER_ID,
+                        COLUMN_BLOCKED_ID
                 }
         )
 }
@@ -28,15 +30,15 @@ public class Block extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = BlockTableConstants.COLUMN_BLOCK_ID)
+    @Column(name = COLUMN_BLOCK_ID)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = BlockTableConstants.COLUMN_BLOCKER_ID, nullable = false)
+    @JoinColumn(name = COLUMN_BLOCKER_ID, nullable = false)
     private User blocker;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = BlockTableConstants.COLUMN_BLOCKED_ID, nullable = false)
+    @JoinColumn(name = COLUMN_BLOCKED_ID, nullable = false)
     private User blocked;
 
     public static Block create(User blocker, User blocked) {
