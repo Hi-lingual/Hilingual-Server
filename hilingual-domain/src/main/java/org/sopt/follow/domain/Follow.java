322b6edb21bf7b0515d2015907085e14005c1c35
JIEUNI <jieundaeun@naver.com>
2025-08-21 23:31:39 +0900

    [refac] #174 unique 제약 네이밍 추가

--- hilingual-domain/src/main/java/org/sopt/follow/domain/Follow.java
+++ hilingual-domain/src/main/java/org/sopt/follow/domain/Follow.java
@@ -14,6 +14,7 @@ import static org.sopt.follow.domain.FollowTableConstants.*;
         name = TABLE_FOLLOW,
         uniqueConstraints = {
                 @UniqueConstraint(
+                        name = UK_FOLLOWER_FOLLOWEE,
                         columnNames = {
                                 COLUMN_FOLLOWER_ID,
                                 COLUMN_FOLLOWEE_ID
