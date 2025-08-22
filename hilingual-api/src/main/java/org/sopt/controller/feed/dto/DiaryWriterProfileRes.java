52e676537e94e8e31e58636ee1b0da680ed39f23
JIEUNI <jieundaeun@naver.com>
2025-08-22 16:07:17 +0900

    [feat] #182 DiaryWriterProfileRes Dto 구현

---
+++ hilingual-api/src/main/java/org/sopt/controller/feed/dto/DiaryWriterProfileRes.java
@@ -0,0 +1,57 @@
+package org.sopt.controller.feed.dto;
+
+import org.sopt.diary.domain.Diary;
+import org.sopt.userprofile.domain.UserProfile;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public record DiaryWriterProfileRes(
+        Boolean isMine,
+        Profile profile,
+        FeedDiary diary
+) {
+    public static DiaryWriterProfileRes of(
+            final Diary diary,
+            final boolean isMine,
+            final boolean isLiked) {
+        return new DiaryWriterProfileRes(
+                isMine,
+                Profile.of(diary.getUser().getUserProfile()),
+                FeedDiary.of(diary, isLiked)
+        );
+    }
+    record Profile(
+            Long userId,
+            String profileImg,
+            String nickname,
+            Integer streak
+    ){
+        static Profile of(final UserProfile userProfile) {
+            return new Profile(
+                    userProfile.getUser().getId(),
+                    (userProfile.getProfileImg() != null) ? userProfile.getProfileImg() : "",
+                    userProfile.getNickname(),
+                    userProfile.getStreak()
+            );
+        }
+    }
+
+    record FeedDiary(
+            Long sharedDate,
+            Integer likeCount,
+            Boolean isLiked
+    ){
+        static FeedDiary of(final Diary diary, final boolean isLiked) {
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime createdAt = diary.getCreatedAt();
+            long minutesDiff = Duration.between(createdAt, now).toMinutes();
+
+            return new FeedDiary(
+                    (minutesDiff < 1) ? 0L : minutesDiff,
+                    diary.getIsLiked(),
+                    isLiked
+            );
+        }
+    }
+}
