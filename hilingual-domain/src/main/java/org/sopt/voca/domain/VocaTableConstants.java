a3fbce007971f6b1b59b64f333c9a8b0be2d2ff6
choyeongju <jjudy1301@naver.com>
2025-08-26 17:47:30 +0900

    [feat] #188 UK 제약조건 상수 필드 추가

--- hilingual-domain/src/main/java/org/sopt/voca/domain/VocaTableConstants.java
+++ hilingual-domain/src/main/java/org/sopt/voca/domain/VocaTableConstants.java
@@ -2,6 +2,7 @@ package org.sopt.voca.domain;
 
 public class VocaTableConstants {
     public static final String TABLE_VOCA = "voca";
+    public static final String UK_VOCA_USER_RECOMMEND = "uk_voca_user_recommend";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_USER_ID = "user_id";
     public static final String COLUMN_RECOMMEND_ID = "recommend_id";
