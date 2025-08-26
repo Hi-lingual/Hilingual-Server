43cc62fadce7931a09d07434966505d52fbfa311
choyeongju <jjudy1301@naver.com>
2025-08-26 17:47:14 +0900

    [feat] #188 유니크 제약조건 추가, 매핑 관계 변경, static 메소드 추가

--- hilingual-domain/src/main/java/org/sopt/voca/domain/Voca.java
+++ hilingual-domain/src/main/java/org/sopt/voca/domain/Voca.java
@@ -11,8 +11,18 @@ import org.sopt.user.domain.User;
 import org.sopt.voca.type.SavedRoot;
 import org.sopt.voca.type.SavedRootConverter;
 
+import static org.sopt.voca.domain.VocaTableConstants.*;
+
 @Entity
-@Table(name = VocaTableConstants.TABLE_VOCA)
+@Table(
+        name = TABLE_VOCA,
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = UK_VOCA_USER_RECOMMEND,
+                        columnNames = { COLUMN_USER_ID, COLUMN_RECOMMEND_ID }
+                )
+        }
+)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -20,23 +30,27 @@ public class Voca extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = VocaTableConstants.COLUMN_ID)
+    @Column(name = COLUMN_ID)
     private Long id;
 
     @Convert(converter = SavedRootConverter.class)
-    @Column(name = VocaTableConstants.COLUMN_SAVED_ROOT, nullable = false)
+    @Column(name = COLUMN_SAVED_ROOT, nullable = false)
     private SavedRoot savedRoot;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = VocaTableConstants.COLUMN_USER_ID, nullable = false)
+    @JoinColumn(name = COLUMN_USER_ID, nullable = false)
     private User user;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = VocaTableConstants.COLUMN_RECOMMEND_ID, nullable = false, unique = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = COLUMN_RECOMMEND_ID, nullable = false)
     private Recommend recommend;
 
-    public Voca(User user, Recommend recommend) {
-        this.user = user;
-        this.recommend = recommend;
+    public static Voca fromMyDiary(User user, Recommend recommend) {
+        return new Voca(null, SavedRoot.MY, user, recommend);
     }
+
+    public static Voca fromFeed(User user, Recommend recommend) {
+        return new Voca(null, SavedRoot.FEED, user, recommend);
+    }
+
 }
