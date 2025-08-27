54e5bac3e7ed1fd7f3625a55a9ef979adc9675c6
JIEUNI <jieundaeun@naver.com>
2025-08-28 02:40:40 +0900

    [feat] #198 UserProfileSuccessCode 구현

--- hilingual-api/src/main/java/org/sopt/controller/userprofile/exception/UserProfileSuccessCode.java
+++ hilingual-api/src/main/java/org/sopt/controller/userprofile/exception/UserProfileSuccessCode.java
@@ -1,15 +1,16 @@
-package org.sopt.controller.user.exception;
+package org.sopt.controller.userprofile.exception;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.exception.code.SuccessCode;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
-public enum UserSuccessCode implements SuccessCode {
+public enum UserProfileSuccessCode implements SuccessCode {
     NICKNAME_AVAILABLE(HttpStatus.OK, 20000, "사용 가능한 닉네임입니다."),
     NICKNAME_DUPLICATED(HttpStatus.OK, 20001, "중복된 닉네임입니다."),
     NICKNAME_SPECIAL_SYMBOLS(HttpStatus.OK, 20002, "특수문자, 이모지가 포함된 닉네임입니다."),
-    NICKNAME_COUNT(HttpStatus.OK, 20003, "2자 미만이거나 10자 초과된 닉네임입니다.");
+    NICKNAME_COUNT(HttpStatus.OK, 20003, "2자 미만이거나 10자 초과된 닉네임입니다."),
+    NICKNAME_FORBIDDEN(HttpStatus.OK, 20004, "금지어가 포함된 닉네임입니다.");
 
     public final HttpStatus httpStatus;
     private final int code;
