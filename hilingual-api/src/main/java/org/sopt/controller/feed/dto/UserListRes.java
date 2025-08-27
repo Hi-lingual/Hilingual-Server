417a1c65e6f16458571d8227f1e55dbb242c9434
JIEUNI <jieundaeun@naver.com>
2025-08-28 01:47:58 +0900

    [feat] #196 UserListRes Dto 구현

---
+++ hilingual-api/src/main/java/org/sopt/controller/feed/dto/UserListRes.java
@@ -0,0 +1,35 @@
+package org.sopt.controller.feed.dto;
+
+import org.sopt.userprofile.dto.UserSearchProjection;
+
+import java.util.List;
+
+public record UserListRes(
+        List<SearchUser> userList
+) {
+    public static UserListRes of(final List<UserSearchProjection> userList) {
+        List<SearchUser> searchUserList = userList.stream()
+                .map(SearchUser::of)
+                .toList();
+
+        return new UserListRes(searchUserList);
+    }
+
+    record SearchUser(
+            Long userId,
+            String profileImg,
+            String nickname,
+            Boolean isFollowing,
+            Boolean isFollowed
+    ){
+        static SearchUser of(UserSearchProjection userList) {
+            return new SearchUser(
+                    userList.getUserId(),
+                    userList.getProfileImg(),
+                    userList.getNickname(),
+                    userList.getIsFollowing(),
+                    userList.getIsFollowed()
+            );
+        }
+    }
+}
