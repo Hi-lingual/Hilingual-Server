d22e3b1aa9271b502cf965a2e99cd3451fd85386
choyeongju <jjudy1301@naver.com>
2025-08-27 18:02:37 +0900

    [feat] #191 Facade

--- hilingual-domain/src/main/java/org/sopt/alarmpreference/facade/AlarmPreferenceFacade.java
+++ hilingual-domain/src/main/java/org/sopt/alarmpreference/facade/AlarmPreferenceFacade.java
@@ -2,17 +2,30 @@ package org.sopt.alarmpreference.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.alarmpreference.domain.AlarmPreference;
+import org.sopt.alarmpreference.type.AlarmType;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AlarmPreferenceFacade {
 
+    private final AlarmPreferenceRetriever alarmPreferenceRetriever;
+    private final AlarmPreferenceUpdater alarmPreferenceUpdater;
     private final AlarmPreferenceSaver alarmPreferenceSaver;
 
-    /*
-     * Saver
-     */
+    public Map<AlarmType, Boolean> getAlarmStatusMap(final long userId) {
+        return alarmPreferenceRetriever.getAlarmStatusMap(userId);
+    }
+
+    @Transactional
+    public void toggle(final long userId, final AlarmType type) {
+        alarmPreferenceUpdater.toggle(userId, type);
+    }
+
     public AlarmPreference save(AlarmPreference alarmPreference) {
         return alarmPreferenceSaver.save(alarmPreference);
     }
