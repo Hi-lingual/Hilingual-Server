7958c754b0e3aad49b1b6ee57a1213708a9e497d
choyeongju <jjudy1301@naver.com>
2025-09-06 00:30:38 +0900

    [feat] #247 공지 생성 API

--- hilingual-api/src/main/java/org/sopt/controller/admin/api/AdminController.java
+++ hilingual-api/src/main/java/org/sopt/controller/admin/api/AdminController.java
@@ -2,16 +2,12 @@ package org.sopt.controller.admin.api;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.sopt.controller.admin.dto.NoticeCreateReq;
 import org.sopt.controller.admin.dto.NoticeCreateRes;
 import org.sopt.controller.admin.service.AdminNoticeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,13 +16,21 @@ public class AdminController {
 
     private final AdminNoticeService adminNoticeService;
 
-    @PostMapping("/notices")
     @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/notices")
     public ResponseEntity<NoticeCreateRes> create(
             @Valid @RequestBody NoticeCreateReq req
     ) {
         return ResponseEntity.ok(adminNoticeService.create(req));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/notices/{noticeId}/delivery")
+    public ResponseEntity<Void> deliver(
+            @PathVariable Long noticeId
+    ) {
+        adminNoticeService.deliver(noticeId);
+        return ResponseEntity.ok().build();
+    }
 }
 
