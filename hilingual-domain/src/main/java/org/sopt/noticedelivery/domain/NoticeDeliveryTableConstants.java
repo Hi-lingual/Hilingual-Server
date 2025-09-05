69d1e086016557d3491bf396a24714e2b65a448b
choyeongju <jjudy1301@naver.com>
2025-09-06 00:31:21 +0900

    [feat] #247 유니크 제약 상수 정의

--- hilingual-domain/src/main/java/org/sopt/noticedelivery/domain/NoticeDeliveryTableConstants.java
+++ hilingual-domain/src/main/java/org/sopt/noticedelivery/domain/NoticeDeliveryTableConstants.java
@@ -7,4 +7,7 @@ public class NoticeDeliveryTableConstants {
     public static final String COLUMN_USER_ID = "user_id";
     public static final String COLUMN_DELIVERED_AT = "delivered_at";
     public static final String COLUMN_READ_AT = "read_at";
+
+    // Unique Constraints
+    public static final String UK_NOTICE_DELIVERY_NOTICE_USER = "uk_notice_delivery_notice_user";
 }
