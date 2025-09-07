777eef3c35f50566db046eae6908204630c45f66
choyeongju <jjudy1301@naver.com>
2025-09-07 17:22:34 +0900

    [fix] #255 사용자 북마크 여부 포함한 Recommend 조회 쿼리 추가

--- hilingual-domain/src/main/java/org/sopt/recommend/repository/RecommendRepository.java
+++ hilingual-domain/src/main/java/org/sopt/recommend/repository/RecommendRepository.java
@@ -1,22 +1,32 @@
 package org.sopt.recommend.repository;
 
 import org.sopt.recommend.domain.Recommend;
+import org.sopt.recommend.dto.RecommendWithBookmarkDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RecommendRepository extends JpaRepository<Recommend, Long> {
 
     List<Recommend> findByDiaryId(Long diaryId);
 
     @Query("""
-    SELECT r FROM Recommend r
-    JOIN FETCH r.diary d
-    WHERE r.id = :phraseId AND d.user.id = :userId
+    select new org.sopt.recommend.dto.RecommendWithBookmarkDto(
+        r.id,
+        r.phraseType,
+        r.phrase,
+        r.explanation,
+        r.reason,
+        case when (v.id is not null) then true else false end
+    )
+    from Recommend r
+    left join Voca v on v.recommendId = r.id and v.user.id = :userId
+    where r.diary.id = :diaryId
 """)
-    Optional<Recommend> findPhraseByIdAndUserId(@Param("phraseId") Long phraseId,
-                                                @Param("userId") Long userId);
+    List<RecommendWithBookmarkDto> findWithBookmarkFlag(
+            @Param("userId") Long userId,
+            @Param("diaryId") Long diaryId
+    );
 }
