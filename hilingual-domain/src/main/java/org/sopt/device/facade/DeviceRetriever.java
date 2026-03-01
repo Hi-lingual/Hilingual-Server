ac258eedd1372927b99f373790d38709c32fd4a2
JIEUNI <jieundaeun@naver.com>
2026-03-02 03:16:33 +0900

    [feat] #320 DeviceRetriever 구현

--- hilingual-domain/src/main/java/org/sopt/device/facade/DeviceRetriever.java
+++ hilingual-domain/src/main/java/org/sopt/device/facade/DeviceRetriever.java
@@ -1,4 +1,19 @@
 package org.sopt.device.facade;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.device.domain.Device;
+import org.sopt.device.repository.DeviceRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
 public class DeviceRetriever {
+
+    private final DeviceRepository deviceRepository;
+
+    public Optional<Device> findByUserIdAndUuid(final long userId, final String uuid) {
+        return deviceRepository.findByUser_IdAndUuid(userId, uuid);
+    }
 }
