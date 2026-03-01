20fa2c17169dbbb7c9e6ca0480de58a4d4e0ec38
JIEUNI <jieundaeun@naver.com>
2026-03-01 22:23:35 +0900

    [feat] #320 DeviceTableConstants 생성

---
+++ hilingual-domain/src/main/java/org/sopt/device/domain/DeviceTableConstants.java
@@ -0,0 +1,19 @@
+package org.sopt.device.domain;
+
+public class DeviceTableConstants {
+    public static final String TABLE_DEVICE = "device";
+    public static final String UK_USER_ID_UUID = "uk_user_id_uuid";
+
+    public static final String COLUMN_DEVICE_ID = "device_id";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_UUID = "uuid";
+
+    public static final String COLUMN_TIMEZONE = "timezone";
+    public static final String COLUMN_DEVICE_NAME = "device_name";
+    public static final String COLUMN_DEVICE_TYPE = "device_type";
+
+    public static final String COLUMN_OS_NAME = "os_name";
+    public static final String COLUMN_OS_TYPE = "os_type";
+    public static final String COLUMN_OS_VERSION = "os_version";
+    public static final String COLUMN_APP_VERSION = "app_version";
+}
