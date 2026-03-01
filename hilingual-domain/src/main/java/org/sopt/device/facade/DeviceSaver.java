23bc4d59f0ecd841c51c0dc548b4b4d022b014eb
JIEUNI <jieundaeun@naver.com>
2026-03-02 03:16:41 +0900

    [feat] #320 DeviceSaver 구현

--- hilingual-domain/src/main/java/org/sopt/device/facade/DeviceSaver.java
+++ hilingual-domain/src/main/java/org/sopt/device/facade/DeviceSaver.java
@@ -1,4 +1,18 @@
 package org.sopt.device.facade;
 
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.device.domain.Device;
+import org.sopt.device.repository.DeviceRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DeviceSaver {
+
+    private final DeviceRepository deviceRepository;
+
+    public void save(Device device) {
+        deviceRepository.save(device);
+    }
 }
