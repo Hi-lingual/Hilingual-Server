d2c070a282e2e3226fda32d2c88bad3a6363a3c1
choyeongju <jjudy1301@naver.com>
2026-03-26 13:36:21 +0900

    [fix] open-in-view false 전환 대비 트랜잭션 누락 수정

--- hilingual-api/src/main/java/org/sopt/controller/feed/service/FeedService.java
+++ hilingual-api/src/main/java/org/sopt/controller/feed/service/FeedService.java
@@ -90,6 +90,7 @@ public class FeedService {
         );
     }
 
+    @Transactional(readOnly = true)
     public SharedDiaryListRes getSharedDiaries(Long userId, Long targetUserId) {
         // 유저 프로필 조회
         UserProfile userProfile = userFacade.getUserById(targetUserId).getUserProfile();
