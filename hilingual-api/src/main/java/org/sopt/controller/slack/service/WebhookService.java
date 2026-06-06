12b73ed6c1965710166c0667438cf56f34c10a74
JIEUNI <jieundaeun@naver.com>
2026-06-07 08:09:45 +0900

    [chore] #387 WebhookService slack버전으로 수정

--- hilingual-api/src/main/java/org/sopt/controller/slack/service/WebhookService.java
+++ hilingual-api/src/main/java/org/sopt/controller/slack/service/WebhookService.java
@@ -1,8 +1,6 @@
-package org.sopt.controller.discord.service;
+package org.sopt.controller.slack.service;
 
 import lombok.RequiredArgsConstructor;
-import org.sopt.user.domain.User;
-import org.sopt.user.facade.UserFacade;
 import org.sopt.userprofile.domain.UserProfile;
 import org.sopt.userprofile.facade.UserProfileFacade;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,8 +19,8 @@ import java.util.Map;
 @Transactional
 public class WebhookService {
 
-    @Value("${discord.webhook.url}")
-    private String discordWebhookUrl;
+    @Value("${slack.webhook.url}")
+    private String slackWebhookUrl;
 
     private final UserProfileFacade userProfileFacade;
 
@@ -30,19 +28,19 @@ public class WebhookService {
         final long totalMembers = userProfileFacade.count();
 
         String message = "✏️하이링구얼 신규 회원✏️\n🔸닉네임 : " + userProfile.getNickname() + "\n🔸현재 가입 유저 수 : " + totalMembers + "명\n🔸플랫폼 : " + userProfile.getUser().getProvider();
-        sendDiscordWebhook(message);
+        sendSlackWebhook(message);
     }
 
-    private void sendDiscordWebhook(String message) {
+    private void sendSlackWebhook(String message) {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> body = new HashMap<>();
-        body.put("content", message);
+        body.put("text", message);
 
         HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(body, headers);
-        restTemplate.postForEntity(discordWebhookUrl, requestEntity, String.class);
+        restTemplate.postForEntity(slackWebhookUrl, requestEntity, String.class);
     }
 }
