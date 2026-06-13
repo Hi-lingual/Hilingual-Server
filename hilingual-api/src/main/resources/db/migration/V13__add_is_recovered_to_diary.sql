0b447dcfb0fe8c98ca5e175c92531f8e9b190f66
JIEUNI <jieundaeun@naver.com>
2026-06-13 14:16:56 +0900

    [chore] #392 Flyway 스크립트에 방어로직 추가

--- hilingual-api/src/main/resources/db/migration/V13__add_is_recovered_to_diary.sql
+++ hilingual-api/src/main/resources/db/migration/V13__add_is_recovered_to_diary.sql
@@ -1,4 +1,4 @@
 -- V13__add_is_recovered_to_diary.sql
 
 alter table diary
-    add column is_recovered boolean not null default false;
+    add column if not exists is_recovered boolean not null default false;
