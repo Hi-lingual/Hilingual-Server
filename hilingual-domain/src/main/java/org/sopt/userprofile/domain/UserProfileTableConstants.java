231760a8949a1e074c5663070678565423293296
JIEUNI <jieundaeun@naver.com>
2026-06-13 11:40:15 +0900

    [add] #392 UserProfile 테이블에 스트릭 부활권 개수 필드 추가

--- hilingual-domain/src/main/java/org/sopt/userprofile/domain/UserProfileTableConstants.java
+++ hilingual-domain/src/main/java/org/sopt/userprofile/domain/UserProfileTableConstants.java
@@ -10,5 +10,6 @@ public class UserProfileTableConstants {
     public static final String COLUMN_USER_ID = "user_id";
     public static final String COLUMN_FOLLOWING_COUNT = "user_following";
     public static final String COLUMN_FOLLOWER_COUNT = "user_follower";
+    public static final String COLUMN_RECOVERY_CHANCE_COUNT = "recovery_chance";
     public static final String DEFAULT_PROFILE_IMG = " ";
 }
