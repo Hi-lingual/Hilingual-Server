adb35dfe2120f5a8d9050e7ade1dd35c05e5950d
JIEUNI <jieundaeun@naver.com>
2026-06-13 12:12:28 +0900

    [add] #392 RecoveryTicket 테이블 추가

---
+++ hilingual-domain/src/main/java/org/sopt/recoveryticket/domain/RecoveryTicket.java
@@ -0,0 +1,50 @@
+package org.sopt.recoveryticket.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.common.config.BaseTimeEntity;
+import org.sopt.user.domain.User;
+
+import java.time.LocalDate;
+
+import static org.sopt.recoveryticket.domain.RecoveryTicketTableConstants.*;
+import static org.sopt.recoveryticket.domain.RecoveryTicketTableConstants.UK_RECOVERY_TICKET_USER_WRITTEN_DATE;
+
+@Entity
+@Table(
+        name = RecoveryTicketTableConstants.TABLE_RECOVERY_TICKET,
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = UK_RECOVERY_TICKET_USER_WRITTEN_DATE,
+                        columnNames = {COLUMN_USER_ID, COLUMN_WRITTEN_DATE}
+                )
+        }
+)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class RecoveryTicket extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = COLUMN_ID)
+    private Long id;
+
+    @Column(name = COLUMN_WRITTEN_DATE, nullable = false)
+    private LocalDate writtenDate;
+
+    @Column(name = COLUMN_IS_USED, nullable = false)
+    private Boolean isUsed = Boolean.FALSE; // 광고 시청 O, 일기 작성 X인 상태
+    // isUsed = true인 경우 광고 시청 O, 일기 작성 O인 상태
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = COLUMN_USER_ID, nullable = false)
+    private User user;
+
+    public void markIsUsed() {
+        this.isUsed = true;
+    }
+}
---
+++ hilingual-domain/src/main/java/org/sopt/recoveryticket/domain/RecoveryTicketTableConstants.java
@@ -0,0 +1,11 @@
+package org.sopt.recoveryticket.domain;
+
+public class RecoveryTicketTableConstants {
+    public static final String TABLE_RECOVERY_TICKET = "recovery_ticket";
+    public static final String UK_RECOVERY_TICKET_USER_WRITTEN_DATE = "uk_recovery_ticket_user_written_date";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_WRITTEN_DATE = "written_date";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_IS_USED = "is_used"; // 실제 일기를 작성했는지 여부
+}
