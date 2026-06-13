9c20fe318c4c6ebf6ec3a860e84ef3eef84d14ba
JIEUNI <jieundaeun@naver.com>
2026-06-13 15:56:01 +0900

    [feat] #392 [스트릭 부활 전용] 일기 피드백 요청 API 로직 구현

--- hilingual-domain/src/main/java/org/sopt/recoveryticket/facade/RecoveryTicketFacade.java
+++ hilingual-domain/src/main/java/org/sopt/recoveryticket/facade/RecoveryTicketFacade.java
@@ -1,10 +1,20 @@
 package org.sopt.recoveryticket.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.recoveryticket.domain.RecoveryTicket;
+import org.sopt.recoveryticket.exception.RecoveryTicketCoreErrorCode;
+import org.sopt.recoveryticket.exception.RecoveryTicketNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
 public class RecoveryTicketFacade {
+    private final RecoveryTicketRetriever recoveryTicketRetriever;
 
+    public RecoveryTicket getValidTicket(final Long userId, final LocalDate writtenDate) {
+        return recoveryTicketRetriever.findValidTicket(userId, writtenDate)
+                .orElseThrow(() -> new RecoveryTicketNotFoundException(RecoveryTicketCoreErrorCode.RECOVERY_TICKET_NOT_FOUND));
+    }
 }
--- hilingual-domain/src/main/java/org/sopt/recoveryticket/repository/RecoveryTicketRepository.java
+++ hilingual-domain/src/main/java/org/sopt/recoveryticket/repository/RecoveryTicketRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface RecoveryTicketRepository extends JpaRepository<RecoveryTicket, Long> {
@@ -21,6 +22,18 @@ public interface RecoveryTicketRepository extends JpaRepository<RecoveryTicket, 
             @Param("userId") Long userId,
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate
+    );
+
+    // 특정 날짜의 미사용 티켓 존재하는지 조회
+    @Query("""
+        SELECT rt FROM RecoveryTicket rt
+        WHERE rt.user.id = :userId
+          AND rt.writtenDate = :writtenDate
+          AND rt.isUsed = false
+    """)
+    Optional<RecoveryTicket> findByUserIdAndWrittenDateAndIsUsedFalse(
+            @Param("userId") Long userId,
+            @Param("writtenDate") LocalDate writtenDate
     );
 }
 
