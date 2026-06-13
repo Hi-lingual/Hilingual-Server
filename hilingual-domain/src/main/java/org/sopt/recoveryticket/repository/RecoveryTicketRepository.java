4243ed1773a83a96827e684c2e2d6b388d2993a4
JIEUNI <jieundaeun@naver.com>
2026-06-13 15:32:48 +0900

    [feat] #392 광고 시청 작성하지 않은 일기 조회 로직 구현

--- hilingual-domain/src/main/java/org/sopt/recoveryticket/repository/RecoveryTicketRepository.java
+++ hilingual-domain/src/main/java/org/sopt/recoveryticket/repository/RecoveryTicketRepository.java
@@ -2,9 +2,25 @@ package org.sopt.recoveryticket.repository;
 
 import org.sopt.recoveryticket.domain.RecoveryTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 public interface RecoveryTicketRepository extends JpaRepository<RecoveryTicket, Long> {
 
+    @Query("""
+        SELECT rt FROM RecoveryTicket rt
+        WHERE rt.user.id = :userId
+          AND rt.isUsed = false
+          AND rt.writtenDate BETWEEN :startDate AND :endDate
+    """)
+    List<RecoveryTicket> findUnlockedTicketsByUserIdAndDateRange(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
 
