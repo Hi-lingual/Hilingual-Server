4018bc9898e3e8a4fb48e5808cbf8abd9b5ea23f
JIEUNI <jieundaeun@naver.com>
2026-06-19 03:43:00 +0900

    [del] #404 불필요한 주석 삭제

--- hilingual-api/src/main/resources/db/migration/V16__update_recovery_ticket_cascade.sql
+++ hilingual-api/src/main/resources/db/migration/V16__update_recovery_ticket_cascade.sql
@@ -3,7 +3,6 @@
 ALTER TABLE recovery_ticket
     DROP CONSTRAINT IF EXISTS recovery_ticket_user_id_fkey;
 
--- 2. ON DELETE CASCADE가 포함된 새로운 외래 키 제약조건을 추가합니다.
 ALTER TABLE recovery_ticket
     ADD CONSTRAINT recovery_ticket_user_id_fkey
         FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
